248b3c47b771bd4b75404526e90709d558706a24
AndrewLi <13058100289@163.com>
2019-12-21 17:16:25 +0800

    use element-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ElementUI from 'element-ui';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/less/font-awesome.less";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App)
